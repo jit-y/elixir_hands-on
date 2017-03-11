56a7fcfdc5e76e15ed32d751c82284ecffba2218
yuji takahashi <jit.y.31@gmail.com>
2017-03-11 15:35:10 +0900

    ext

--- elixir_handson_demo/lib/demo/hello_handler.ex
+++ elixir_handson_demo/lib/demo/hello_handler.ex
@@ -1,9 +1,27 @@
 defmodule Demo.HelloHandler do
   def init(req, opts) do
-    IO.puts "hoge"
-    headers = %{"content-type" => "text/plain"}
-    body = "Hello, Cowboy Handler"
-    req2 = :cowboy_req.reply(200, headers, body, req)
+    handle(req, opts)
+  end
+
+  defp handle(req, opts) do
+    headers = %{"content-type" => "text/html"}
+    req2 = :cowboy_req.reply(200, headers, build_body(), req)
     {:ok, req2, opts}
+  end
+
+  defp build_body do
+    """
+    <html lang="ja">
+      <head>
+        <meta charset="utf-8">
+        <title>Sample Static</title>
+      </head>
+      <body>
+        <h1>Cowboy</h1>
+        <div class="root"></div>
+      </body>
+    </html>
+
+    """
   end
 end
